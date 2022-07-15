2095bbce63c4a1be1fbbffc78134a01c41261208
ooflet <104404175+ooflet@users.noreply.github.com>
2022-07-15 20:28:52 +1000

    Update cmds.lua

--- framework/help/cmds.lua
+++ framework/help/cmds.lua
@@ -6,7 +6,7 @@ Commands["system"] = {
 	{"console", "<function> - Opens the default Roblox Developer Console"}
 }
 Commands["special"] = { 
-	{"install", "<function>, <value> - Fetches, downloads, and executes a module from the GitHub repo"},
-  {"executemodule", "<function>, <value> - Fetches and executes a module from the GitHub repo"},
-  {"serverhop/safehop", "<function> - Disconnects and rejoins the player into potentially a different server"}
+	{"install", "<function>, <value> - Fetches, downloads, and executes a module from the GitHub repo"},	
+	{"executemodule", "<function>, <value> - Fetches and executes a module from the GitHub repo"},	
+	{"serverhop/safehop", "<function> - Disconnects and rejoins the player into potentially a different server"}
 }
