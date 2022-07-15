1cd712eb818e88fcf082e47d4e71f5a019e668f5
ooflet <104404175+ooflet@users.noreply.github.com>
2022-07-15 20:11:07 +1000

    Added new commands to cmds.lua

---
+++ framework/help/cmds.lua
@@ -0,0 +1,12 @@
+local Commands = {}
+Commands["settings"] = { 
+	{"blur", "<setting>, <boolean> - Changes the background blur seen on the UI"}
+}
+Commands["system"] = { 
+	{"console", "<function> - Opens the default Roblox Developer Console"}
+}
+Commands["special"] = { 
+	{"install", "<function>, <value> - Fetches, downloads, and executes a module from the GitHub repo"},
+  {"executemodule", "<function>, <value> - Fetches and executes a module from the GitHub repo"},
+  {"serverhop/safehop", "<function> - Disconnects and rejoins the player into potentially a different server"}
+}
