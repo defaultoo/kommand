443206a87d45c05bafcc9cc44158f51056f07d9f
ooflet <104404175+ooflet@users.noreply.github.com>
2022-07-22 10:35:59 +1000

    Update cmds.lua

--- framework/help/cmds.lua
+++ framework/help/cmds.lua
@@ -1,8 +1,13 @@
 local Commands = {}
+Commands["kommand"] = { 
+	{"dex", "<function>, Opens the built in dex explorer utilising lib:Dex(). It installs and opens just like a normal module."}
+	{"remotespy", "<function>, Opens the built in remotespy utilising lib:RemoteSpy(). It installs and opens just like a normal module."}	
+}
 Commands["settings"] = { 
 	{"blur", "<setting>, <boolean> - Changes the background blur seen on the UI"}
 }
 Commands["system"] = { 
+	{"
 	{"console", "<function> - Opens the default Roblox Developer Console"}
 }
 Commands["special"] = { 
