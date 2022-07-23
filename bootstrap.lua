01621961bde27108b4fe9f66a5756dfa35d6133b
ooflet <104404175+ooflet@users.noreply.github.com>
2022-07-23 13:50:28 +1000

    Update bootstrap.lua

--- bootstrap.lua
+++ bootstrap.lua
@@ -2,7 +2,6 @@ rconsoleclear()
 rconsolename("Bootstrapper")
 rconsoleprint("Checking Kommand Framework\n")
 
-task.wait(1)
 if isfile("kommand/framework/kommandframework.kmd") then
 	if readfile("kommand/framework/kommandframework.kmd") == game:HttpGet("https://raw.githubusercontent.com/ooflet/kommand/main/framework/KommandFramework.lua") then
 		rconsoleprint("Checking Kommand Library\n")
@@ -10,8 +9,7 @@ if isfile("kommand/framework/kommandframework.kmd") then
 		rconsoleprint("Framework Update Found\n")
 		rconsoleprint("Updating Kommand Framework\n")
 		delfile("kommand/framework/kommandframework.kmd")
-		writefile("kommand/framework/kommandframework.kmd", game:HttpGet("https://raw.githubusercontent.com/ooflet/kommand/main/framework/KommandFramework.lua"))
-		wait(0.5)	
+		writefile("kommand/framework/kommandframework.kmd", game:HttpGet("https://raw.githubusercontent.com/ooflet/kommand/main/framework/KommandFramework.lua"))	
 		rconsoleprint("Checking Kommand Library\n")
 	end
 else
@@ -21,17 +19,13 @@ else
 	writefile("kommand/framework/kommandframework.kmd", game:HttpGet("https://raw.githubusercontent.com/ooflet/kommand/main/framework/KommandFramework.lua"))
 end
 
-wait(0.5)
-
 if isfile("kommand/library/kommandlibrary.kmd") then
 	if readfile("kommand/library/kommandlibrary.kmd") == game:HttpGet("https://raw.githubusercontent.com/ooflet/kommand/main/framework/lib.lua") then
-		wait(0.5)
 	else 
 		rconsoleprint("Library Update Found\n")
 		rconsoleprint("Updating Kommand Library\n")
 		delfile("kommand/library/kommandlibrary.kmd")
 		writefile("kommand/library/kommandlibrary.kmd", game:HttpGet("https://raw.githubusercontent.com/ooflet/kommand/main/framework/lib.lua"))
-		task.wait(0.5)	
 	end
 else
 	rconsoleprint("Installing Kommand Library\n")
@@ -40,6 +34,5 @@ else
 end
 
 rconsoleprint("Launching Kommand\n")
-wait(0.75)
 rconsoleclose()
 loadstring(readfile("kommand/framework/kommandframework.kmd"))()
