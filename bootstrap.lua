13c9ead71027922769c6b2e1b5bb1c5749e25f01
ooflet <104404175+ooflet@users.noreply.github.com>
2022-07-29 15:10:00 +1000

    Update bootstrap.lua

--- bootstrap.lua
+++ bootstrap.lua
@@ -1,37 +1,6 @@
 rconsoleclear()
 rconsolename("Bootstrapper")
 rconsoleprint("Checking Kommand Framework\n")
-local Debug = false
-spawn(function()
-	rconsoleinput()
-	rconsoleprint("Bootstrap Interrupt! Wait for bootstrap to initialize components. If this was a mistake, wait for the '>' to appear and type 'boot'.")
-	Debug = true
-end)
-
-local function KommandTerminal()
-	while wait() do
-		if Debug == false then
-			break
-		else
-			rconsoleprint("\n# ")
-			local command = rconsoleinput()
-			if string.find(command, "boot") then
-				Debug = false	
-			elseif string.find(command, "bootset") then
-				rconsoleprint("\nSet what parameter\n= ")
-				local response = rconsoleinput()
-				if string.find(response, "debug") then
-					rconsoleprint("\nSet it to\n= ")
-					local debugresponse = rconsoleinput()
-					if string.find(debugresponse, "true") then
-						rconsoleprint("\nSet")
-					elseif string.find(debugresponse, "false") then
-						rconsoleprint("\nSet")
-					end
-				end
-		end
-	end
-end
 
 if isfile("kommand/framework/kommandframework.kmd") then
 	if readfile("kommand/framework/kommandframework.kmd") == game:HttpGet("https://raw.githubusercontent.com/ooflet/kommand/main/framework/KommandFramework.lua") then
@@ -64,9 +33,10 @@ else
 	writefile("kommand/library/kommandlibrary.kmd", game:HttpGet("https://raw.githubusercontent.com/ooflet/kommand/main/framework/lib.lua"))
 end
 rconsoleprint("Initializing Kommand Library\n")
-loadstring(readfile("kommand/library/kommandlibrary.kmd"))()
+spawn(function()
+	loadstring(readfile("kommand/library/kommandlibrary.kmd"))()	
+end)
 if Debug == true then
-	KommandTerminal()
 	rconsoleprint("Launching Kommand\n")
 		wait(1)
 	rconsoleclose()
