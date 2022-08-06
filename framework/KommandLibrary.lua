b42dd3cf8ca7a443599f490e18ffd2e9d2294865
ooflet <104404175+ooflet@users.noreply.github.com>
2022-08-06 15:01:18 +1000

    Update KommandLibrary.lua

--- framework/KommandLibrary.lua
+++ framework/KommandLibrary.lua
@@ -9,7 +9,7 @@ _G.KommandLibrary.Info = {}
 _G.KommandLibrary.Debug = {}
 
 local KonsoleExecuted = false
-local ClientLog = game.CoreGui:WaitForChild("Kommand"):WaitForChild("ConsoleWindow"):WaitForChild("Console"):WaitForChild("ClientLog")
+local Ronaco = loadstring(game:HttpGet("https://raw.githubusercontent.com/ooflet/Ronaco-Editor/main/main/vanilla/RonacoMain.lua")
 
 if game.CoreGui:FindFirstChild("Kommand") then
 	KonsoleExecuted = true	
