746d261bda936cac0091e7db2a06edb1e6cfa9d9
ooflet <104404175+ooflet@users.noreply.github.com>
2023-03-16 18:29:39 +1100

    Update bootstrap.lua

--- bootstrap.lua
+++ bootstrap.lua
@@ -3,6 +3,7 @@
 
 -- GUI Setup
 
+local StatusFail = false
 local BootstrapScreenGUI = Instance.new("ScreenGui")
 local Bootstrapper = Instance.new("Frame")
 local Status = Instance.new("TextLabel")
@@ -57,72 +58,81 @@ Fill.Size = UDim2.new(1, 0, 1, 0)
 
 -- Throbber Animation
 spawn(function()
+    while not StatusFail do
         Fill:TweenPosition(UDim2.new(1,0,0,0), Enum.EasingDirection.InOut, Enum.EasingStyle.Quint, 0.5, true)
         wait(0.25)
         Fill.Position = UDim2.new(-1,0,0,0)
         Fill:TweenPosition(UDim2.new(1,0,0,0), Enum.EasingDirection.Out, Enum.EasingStyle.Quint, 0.5, true)
         wait(0.5)
         Fill.Position = UDim2.new(-1,0,0,0)
+    end
 end)
+
+local function ThrobberError()
+    StatusFail = true
+    Fill.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+    Fill.Position = UDim2.new(-1,0,0,0)
+    Fill:TweenPosition(UDim2.new(0,0,0,0), Enum.EasingDirection.Out, Enum.EasingStyle.Quint, 0.5, true)
+    wait(5)
+    Bootstrapper:TweenPosition(UDim2.new(0.5,0,1,50), Enum.EasingDirection.InOUt, Enum.EasingStyle.Quad, 0.5, true)
+    wait(0.5)
+    BootstrapScreenGUI:Destroy()
+end
 
 -- Bootstrap Setup
 Status.Text = "Checking Kommand Framework"
 Bootstrapper:TweenPosition(UDim2.new(0.5,0,1,0), Enum.EasingDirection.InOUt, Enum.EasingStyle.Quad, 0.5, true)
 wait(0.5)
 
-PrintInfo()
-Status.Text = "Checking Kommand Framework\n"
+local framework, message = pcall(function() module = game:HttpGet("https://raw.githubusercontent.com/ooflet/kommand/main/framework/KommandFramework.lua") end)
+if not framework then
+	Status.Text = "Could not fetch data from https://raw.githubusercontent.com/ooflet/kommand/main/framework/KommandFramework.lua. "..message
+	error()
+end
+
+local library, message = pcall(function() module = game:HttpGet("https://raw.githubusercontent.com/ooflet/kommand/main/framework/KommandLibrary.lua") end)
+if not library then
+	Status.Text = "Could not fetch data from https://raw.githubusercontent.com/ooflet/kommand/main/framework/KommandLibrary.lua. "..message
+	error()
+end
 
 if isfile("kommand/framework/kommandframework.kmd") then
-	if readfile("kommand/framework/kommandframework.kmd") == game:HttpGet("https://raw.githubusercontent.com/ooflet/kommand/main/framework/KommandFramework.lua") then
-		PrintSucess()
-		Status.Text = "Framework up-to-date\n" 
-		PrintInfo()
-		Status.Text = "Checking Kommand Library\n" 
-	else 
-		PrintInfo()
-		Status.Text = "Framework Update Found\n" 
-		PrintInfo()
-		Status.Text =("Updating Kommand Framework\n")
+	if readfile("kommand/framework/kommandframework.kmd") ~= game:HttpGet("https://raw.githubusercontent.com/ooflet/kommand/main/framework/KommandFramework.lua") then
+		Status.Text = "Updating Kommand Framework..."
 		delfile("kommand/framework/kommandframework.kmd")
 		writefile("kommand/framework/kommandframework.kmd", game:HttpGet("https://raw.githubusercontent.com/ooflet/kommand/main/framework/KommandFramework.lua"))	
-		PrintSucess()
-		Status.Text =("Framework up-to-date\n")
-		PrintInfo()
-		Status.Text =("Checking Kommand Library\n")
+		Status.Text = "Checking Kommand Library..."
 	end
 else
-	PrintInfo()
-	Status.Text =("Installing Kommand Framework\n")
+	Status.Text = "Installing Kommand Framework..."
 	makefolder("kommand")
 	makefolder("kommand/framework")
 	writefile("kommand/framework/kommandframework.kmd", game:HttpGet("https://raw.githubusercontent.com/ooflet/kommand/main/framework/KommandFramework.lua"))
+    Status.Text = "Checking Kommand Library.."
 end
 
 if isfile("kommand/library/kommandlibrary.kmd") then
-	if readfile("kommand/library/kommandlibrary.kmd") == game:HttpGet("https://raw.githubusercontent.com/ooflet/kommand/main/framework/KommandLibrary.lua") then
-		PrintSucess()
-		Status.Text =("Library up-to-date\n")
-	else 
-		PrintInfo()
-		Status.Text =("Library Update Found\n")
-		PrintInfo()
-		Status.Text =("Updating Kommand Library\n")
+	if readfile("kommand/library/kommandlibrary.kmd") ~= game:HttpGet("https://raw.githubusercontent.com/ooflet/kommand/main/framework/KommandLibrary.lua") then
+		Status.Text = "Updating Kommand Library..."
 		delfile("kommand/library/kommandlibrary.kmd")
 		writefile("kommand/library/kommandlibrary.kmd", game:HttpGet("https://raw.githubusercontent.com/ooflet/kommand/main/framework/KommandLibrary.lua"))
-		PrintSucess()
-		Status.Text =("Library up-to-date\n")
+        Status.Text = "Initializing..."
 	end
 else
-	PrintInfo()
-	Status.Text =("Installing Kommand Library\n")
+	Status.Text = "Installing Kommand Library..."
 	makefolder("kommand/library")
 	writefile("kommand/library/kommandlibrary.kmd", game:HttpGet("https://raw.githubusercontent.com/ooflet/kommand/main/framework/KommandLibrary.lua"))
-	PrintSucess()
-	Status.Text =("Library up-to-date\n")
+    Status.Text = "Initializing..."
 end
 
-if not isfolder("kommand/modules") then
-	makefolder("kommand/modules")
-end
+wait(0.5)
 
+Bootstrapper:TweenPosition(UDim2.new(0.5,0,1,50), Enum.EasingDirection.InOUt, Enum.EasingStyle.Quad, 0.5, true)
+wait(0.5)
+BootstrapScreenGUI:Destroy()
+spawn(function()
+    loadstring(readfile("kommand/library/kommandlibrary.kmd"))()	
+end)
+spawn(function()
+    loadstring(readfile("kommand/framework/kommandframework.kmd"))()		
+end)
